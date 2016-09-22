046fe456eddeb4493e8b88ce907c09bf111b0005
Scott Bromander <scott@primeacademy.io>
2016-09-22 14:48:55 -0500

    angular route hooked up

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,17 @@
-var myApp = angular.module("myApp", []);
+var myApp = angular.module("myApp", ["ngRoute"]);
+
+myApp.config(["$routeProvider", function($routeProvider){
+    $routeProvider.
+      when("/home", {
+        templateUrl: "/views/partials/home.html"
+      }).
+      when("/cat", {
+        templateUrl: "/views/partials/cat.html"
+      }).
+      when("/lizard", {
+        templateUrl: "/views/partials/lizard.html"
+      }).
+      otherwise({
+        redirectTo: "/home"
+      });
+}]);
